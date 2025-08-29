e2458306c4d4c444586072f0fb0df6c4b48f4e96
Brayhan De Aza <brayhandeaza@gmail.com>
2025-08-29 15:03:56 -0400

    refactor

--- README.md
+++
@@ -1,1 +0,0 @@
-# Binomia Notificaion Server
--- src/sockets/global.ts
+++
@@ -1,5 +0,0 @@
-import { Server, Socket } from "socket.io";
-
-export const accountSockets = (io: Server, socket: Socket, clients: Map<string, string>) => {
-
-}
