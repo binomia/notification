9cd734ac526d23503a10033fe1aeeb5eac864eaf
Brayhan De Aza <brayhandeaza@gmail.com>
2025-09-20 14:04:21 -0400

    use ECC instead of RSA

--- src/sockets/index.ts
+++ src/sockets/index.ts
@@ -5,7 +5,6 @@ import { Job, JobJson, Queue, Worker } from "bullmq";
 export const initSocket = async (io: Server) => {
     io.on("connection", (socket) => {
         const clientId = socket.handshake.query.username
-
         if (clientId) {
             socket.join(clientId);
             console.log("a user connected:", socket.handshake.query.username);
