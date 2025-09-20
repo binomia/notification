d64fd275e9dfeb8eb5bd6e9ae75cdc409579aab7
Brayhan De Aza <brayhandeaza@gmail.com>
2025-09-20 13:51:59 -0400

    notification move to bullmq

--- server.ts
+++ server.ts
@@ -1,12 +1,21 @@
 import "dotenv/config"
 import { Server as SocketIOServer } from 'socket.io';
 import { initSocket } from "@/sockets";
-import { server } from "@/server";
 import { Queues } from "@/queues";
+import { NOTIFICATION_SERVER_PORT, PROMETHEUS_PORT } from "@/constants";
+import { Extensions, Server } from "cromio";
 
+
+const server = new Server({
+    port: Number(NOTIFICATION_SERVER_PORT)
+})
+
+server.addExtension(Extensions.serverPrometheusMetrics({
+    port: Number(PROMETHEUS_PORT)
+}));
 
 server.start(async (url: string) => {
-    console.log(`\n[Notification]: worker ${process.pid} is running on ${url}`);
+    console.log(`[Notification]: worker ${process.pid} is running on ${url}`);
     const io = new SocketIOServer({
         cors: {
             origin: "*"
--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,17 +1,41 @@
 import { ZERO_ENCRYPTION_KEY } from "@/constants";
+import Email from "@/email";
+import { sendNotification } from "@/expo";
 import { JobJson } from "bullmq";
-import { AES } from "cryptografia";
+import { AES, HASH } from "cryptografia";
 import { Server } from "socket.io";
-
+import jwt from 'jsonwebtoken';
 
 
 export default class Controller {
-    static executeJob = async ({ data }: JobJson, io: Server): Promise<void> => {
-        try {            
-            const decryptedData = await AES.decryptAsync(JSON.parse(data), ZERO_ENCRYPTION_KEY);            
-            const { transaction, channel, senderSocketRoom, recipientSocketRoom } = JSON.parse(decryptedData);
-            
-            io.to([recipientSocketRoom, senderSocketRoom]).emit(channel, transaction)
+    static executeJob = async ({ data, name }: JobJson, io: Server): Promise<void> => {
+        try {
+            const decryptedData = await AES.decryptAsync(JSON.parse(data), ZERO_ENCRYPTION_KEY);
+
+            switch (true) {
+                case name.includes("newTransactionNotification"): {
+                    await sendNotification(JSON.parse(decryptedData))
+                    break;
+                }
+                case name.includes("transactionNotification"): {
+                    const { transaction, channel, senderSocketRoom, recipientSocketRoom } = JSON.parse(decryptedData);
+                    io.to([recipientSocketRoom, senderSocketRoom]).emit(channel, transaction)
+                    break;
+                }
+                case name.includes("sendVerificationCode"): {
+                    const { email, code } = JSON.parse(decryptedData);
+                    await Email.sendVerificationCode(email, code);
+                    break;
+                }
+                case name.includes("sendEmail"): {
+                    const { to, subject, text, html } = JSON.parse(decryptedData);
+                    await Email.send({ to, subject, text, html })
+                    break;
+                }
+                default:
+                    break;
+            }
+
 
         } catch (error) {
             console.log({ prosessTransaction: error });
